f7eb31df70889769f4f1c4bb3e28a87549d6911e
meowsbits <b5c6@protonmail.com>
2021-01-06 07:46:22 -0600

    Update ci-script.sh

--- ci-script.sh
+++ ci-script.sh
@@ -2,5 +2,5 @@
 
 echo "I always fail..."
 sed -i.bak 's/__SOMEVAR__/myvariable/' .github/issue-on-failure.md
-echo some useful information >> .github/issue-on-failure.md
+echo some useful information | tee -a .github/issue-on-failure.md
 exit 1
